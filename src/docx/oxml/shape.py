3bfc5fb766e9dd88519e3f3d2fcd1f26246bcfff
Jervis Whitley <jtrain@users.noreply.github.com>
2017-05-02 10:47:55 +1000

    Margins should be ints not floats

--- src/docx/oxml/shape.py
+++ src/docx/oxml/shape.py
@@ -154,10 +154,10 @@ class CT_Anchor(CT_Inline):
             anchor.positionV.getchildren()[0].text = positionV
 
         if margin is not None:
-            anchor.set('distT', margin.get('top', 0))
-            anchor.set('distR', margin.get('right', 0))
-            anchor.set('distB', margin.get('bottom', 0))
-            anchor.set('distL', margin.get('left', 0))
+            anchor.set('distT', u"%d" % margin.get('top', 0))
+            anchor.set('distR', u"%d" % margin.get('right', 0))
+            anchor.set('distB', u"%d" % margin.get('bottom', 0))
+            anchor.set('distL', u"%d" % margin.get('left', 0))
 
         if wrap is not None:
             anchor.wrapSquare.set('wrapText', wrap)
